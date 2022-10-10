bb7f2f15aea6c6c748692e5cf12b66aca3daf18e
2814109 <2814109k@gmail.com>
2022-10-10 14:52:03 +0900

    create modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { CircularProgress } from "@chakra-ui/progress";
 import { FC, Suspense } from "react";
 import Table from "./components/Table";
 import { Flex } from "@chakra-ui/react";
+import BasicModal from "./components/Modal";
 const App: FC = () => (
   // centering by chakraUI
-  <Flex align={"center"} justifyContent={"center"}>
-    <Suspense fallback={<CircularProgress isIndeterminate color="green.300" />}>
-      <Table />
-    </Suspense>
-  </Flex>
+  <>
+    <Flex align={"center"} justifyContent={"center"}>
+      <BasicModal />
+    </Flex>
+    <Flex align={"center"} justifyContent={"center"}>
+      <Suspense
+        fallback={<CircularProgress isIndeterminate color="green.300" />}
+      >
+        <Table />
+      </Suspense>
+    </Flex>
+  </>
 );
 
 export default App;
